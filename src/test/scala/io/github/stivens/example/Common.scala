9ded0970748aca3749ff2184df6915d4f9a4758d
stivens <jacekbizub@gmail.com>
2025-08-04 15:57:30 +0200

    fix example

--- src/test/scala/io/github/stivens/example/Common.scala
+++ src/test/scala/io/github/stivens/example/Common.scala
@@ -2,11 +2,20 @@ package io.github.stivens.example
 
 import java.time.LocalDateTime
 
+@SuppressWarnings(
+  Array(
+    "scalafix:DisableSyntax.var"
+  )
+)
 object Common {
-  def sleep(seconds: Int = 30): Unit = {
-    val start = LocalDateTime.now()
+  def sleep(seconds: Int = 15): Unit = {
+    val start         = LocalDateTime.now()
+    var lastYieldTime = start
     while (LocalDateTime.now().isBefore(start.plusSeconds(seconds))) {
-      println("busy waiting")
+      if (LocalDateTime.now().isAfter(lastYieldTime.plusSeconds(1))) {
+        lastYieldTime = LocalDateTime.now()
+        println("busy waiting")
+      }
     }
   }
 }
