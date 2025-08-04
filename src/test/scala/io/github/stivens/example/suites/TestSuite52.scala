06ada55a727ae6ee1246c2e5ef41ff3ded124ea5
stivens <jacekbizub@gmail.com>
2025-08-04 15:04:00 +0200

    fix

---
+++ src/test/scala/io/github/stivens/example/suites/TestSuite52.scala
@@ -0,0 +1,28 @@
+package io.github.stivens.example.suites
+
+import io.github.stivens.example.Common
+import org.scalatest.funsuite.AnyFunSuite
+
+class TestSuite52 extends AnyFunSuite {
+
+  test("sleep some time and pass -- 1") {
+    Common.sleep()
+    assert(true)
+  }
+
+  test("sleep some time and pass -- 2") {
+    Common.sleep()
+    assert(true)
+  }
+
+  test("sleep some time and pass -- 3") {
+    Common.sleep()
+    assert(true)
+  }
+
+  test("sleep some time and pass -- 4") {
+    Common.sleep()
+    assert(true)
+  }
+
+}
